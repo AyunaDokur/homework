eb6906c7b86a0ea62754332bab2f1f5a5eaeed63
Аюна <graciad666@gmail.com>
2024-09-24 17:59:20 +0700

    24.09

--- module_2_2.py
+++ module_2_2.py
@@ -1,18 +1,6 @@
 first = input('Первое число: ')
 second = input('Второе число: ')
 third = input('Третье число: ')
-"""
-while len(first) == 0 or len(second)== 0 or len(third) == 0:
-    if len(first) == 0:
-        print('Первое число не введено, повторите ввод!')
-        first = input('Повторный ввод: ')
-    if len(second) == 0:
-        print('Второе число не введено, повторите ввод!')
-        second = input('Повторный ввод: ')
-    if len(third) == 0:
-        print('Третье число не введено, повторите ввод!')
-        third = input('Повторный ввод: ')
-"""
 if len(first) == 0 or len(second)== 0 or len(third) == 0:
     print('Найдено пустое занчение, повторите попытку!')
 else:
