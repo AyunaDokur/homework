bb528478a86842911f34db83f9fd9c50b4e43e2d
Аюна <graciad666@gmail.com>
2024-09-24 17:53:00 +0700

    20/09/24

---
+++ module_2_2.py
@@ -0,0 +1,24 @@
+first = input('Первое число: ')
+second = input('Второе число: ')
+third = input('Третье число: ')
+"""
+while len(first) == 0 or len(second)== 0 or len(third) == 0:
+    if len(first) == 0:
+        print('Первое число не введено, повторите ввод!')
+        first = input('Повторный ввод: ')
+    if len(second) == 0:
+        print('Второе число не введено, повторите ввод!')
+        second = input('Повторный ввод: ')
+    if len(third) == 0:
+        print('Третье число не введено, повторите ввод!')
+        third = input('Повторный ввод: ')
+"""
+if len(first) == 0 or len(second)== 0 or len(third) == 0:
+    print('Найдено пустое занчение, повторите попытку!')
+else:
+    if first == second == third:
+        print('Найдено 3 совпадения')
+    elif first == second or first == third or second == third:
+        print('Найдено 2 совпадения')
+    else:
+        print('Найдено 0 совпадений')
