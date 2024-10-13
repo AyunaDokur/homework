2ae8a640d1a6fbeb6ab753724e8464348b393776
Ayuna <graciad666@gmail.com>
2024-10-13 19:54:54 +0300

    13.10

---
+++ module 4/module_4_1.py
@@ -0,0 +1,12 @@
+from true_math import divide as true_division
+from fake_math import divide as fake_division
+
+
+result1 = fake_division(69, 3)
+result2 =fake_division(3, 0)
+result3 = true_division(49, 7)
+result4 = true_division(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
