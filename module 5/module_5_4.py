95b0f19e581daad83f1042a72cc053695d37128d
Ayuna <graciad666@gmail.com>
2024-11-04 12:55:08 +0300

    0.11

---
+++ module 5/module_5_4.py
@@ -0,0 +1,28 @@
+
+class House:
+    houses_history = []
+    __instance = None
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        for key, values in kwargs.items():
+            setattr(self, key, values)
+
+    def __del__(self):
+        print(f'{self.args[0]} снесен, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
