47ac64b5a4efb2e4c653a10177e74145787a2e3f
Ayuna <graciad666@gmail.com>
2024-11-12 21:49:31 +0300

    12.11.2024

---
+++ module 7/module_7_2.py
@@ -0,0 +1,25 @@
+def custom_write(file_name, string):
+    number_of_string = 0
+    strings_positions = {}
+    file = open(file_name, 'a', encoding = 'utf-8')
+    for line in string:
+        number_of_string += 1
+        number_of_byte = file.tell()
+        strings_positions[(number_of_string, number_of_byte)] = line
+        file.write(f'\n{line}')
+    file.close()
+    return strings_positions
+
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+
+for elem in result.items():
+  print(elem)
